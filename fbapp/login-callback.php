6a47547b92d0e808df4b8985a136f358e60614ad
richimor9 <r9_93r@hotmail.com>
2016-04-15 12:14:33 -0500

    FB login app

---
+++ fbapp/login-callback.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+require_once __DIR__ . '/src/Facebook/autoload.php';
+
+$fb = new Facebook\Facebook([
+  'app_id' => '1702294176703016',
+  'app_secret' => 'b49f1573d2c652482a6479f7ac0fb8ff',
+  'default_graph_version' => 'v2.5'
+]);
+
+$helper = $fb->getJavaScriptHelper();
+
+try {
+  $accessToken = $helper->getAccessToken();
+  } catch(Facebook\Exceptions\FacebookResponseException $e) {
+    // When Graph returns an error
+    echo 'Graph returned an error: ' . $e->getMessage();
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+    // When validation fails or other local issues
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+}
+
+if (isset($accessToken)) {
+   $fb->setDefaultAccessToken($accessToken);
+
+  try {
+    $requestProfile = $fb->get("/me?fields=name,email");
+    $profile = $requestProfile->getGraphNode()->asArray();
+  } catch(Facebook\Exceptions\FacebookResponseException $e) {
+    // When Graph returns an error
+    echo 'Graph returned an error: ' . $e->getMessage();
+  } catch(Facebook\Exceptions\FacebookSDKException $e) {
+    // When validation fails or other local issues
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  }
+
+  $_SESSION['name'] = $profile['name'];
+  header('location: ../');
+  exit;
+} else {
+    echo "Unauthorized access!!!";
+    exit;
+}
